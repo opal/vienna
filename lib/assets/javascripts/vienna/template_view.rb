4201d5ae0da4c1dcccce407d9f5ac97d61c90207
Adam Beynon <adam.beynon@gmail.com>
2013-02-20 17:08:31 +0000

    Initial TemplateView implementation

---
+++ lib/assets/javascripts/vienna/template_view.rb
@@ -0,0 +1,31 @@
+require 'vienna/view'
+require 'opal/template'
+
+module Vienna
+  class TemplateView < View
+    def self.template(name = nil)
+      if name
+        @template = name
+      elsif @template
+        @template
+      elsif name = self.name
+        @template.name.sub(/View$/, '').demodulize.underscore
+      end
+    end
+
+    def render
+      before_render
+
+      if template = Template[self.class.template]
+        element.html = template.render self
+      end
+
+      after_render
+    end
+
+    def before_render; end
+
+    def after_render; end
+  end
+end
+
