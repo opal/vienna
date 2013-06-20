eaee54cf5858cceaa283ac10fcf008ce52394839
Adam Beynon <adam.beynon@gmail.com>
2013-06-20 12:00:29 +0100

    Initial Vienna::Observable commit with specs
    Vienna::Observable differs from stdlib Observerable as it allows
    individual attributes to be observed using a block as a handler. This
    makes it more powerful when dealing with long-lived apps which need to
    connect models to view properties.

---
+++ opal/vienna/observable.rb
@@ -0,0 +1,40 @@
+module Vienna
+  module Observable
+
+    def observe(attribute, &handler)
+      unless observers = @attr_observers
+        observers = @attr_observers = {}
+        old_values = @attr_old_values = {}
+      end
+
+      unless handlers = observers[attribute]
+        handlers = observers[attribute] = []
+        replace_writer_for(attribute)
+      end
+
+      handlers << handler
+    end
+
+    def replace_writer_for(attribute)
+      if respond_to? "#{attribute}="
+        define_singleton_method("#{attribute}=") do |val|
+          result = super val
+          attribute_did_change(attribute)
+          result
+        end
+      end
+    end
+
+    # Triggers observers for the given attribute. You may call this directly if
+    # needed, but it is generally called automatically for you inside a
+    # replaced setter method.
+    def attribute_did_change(attribute)
+      return unless @attr_observers
+
+      if handlers = @attr_observers[attribute]
+        new_val = __send__(attribute) if respond_to?(attribute)
+        handlers.each { |h| h.call new_val }
+      end
+    end
+  end
+end
---
+++ spec/observable_spec.rb
@@ -0,0 +1,96 @@
+require 'spec_helper'
+require 'vienna/observable'
+
+class ObservableSpec
+  include Vienna::Observable
+
+  attr_accessor :foo, :bar, :baz
+
+  def baz=(b)
+    @baz = b + 10
+  end
+
+  def bar=(b)
+    @bar = b
+    "#bar"
+  end
+end
+
+describe Vienna::Observable do
+
+  let(:object) { ObservableSpec.new }
+
+  describe "#replace_writer_for" do
+    it "still calls the original method using super" do
+      object.replace_writer_for(:foo)
+      object.baz = 100
+      object.baz.should eq(110)
+    end
+
+    it "returns the value from the original method" do
+      object.replace_writer_for(:bar)
+      (object.bar = 32).should eq("#bar")
+    end
+
+    it "does not add a setter unless an existing attribute= method exists" do
+      object.replace_writer_for(:bob)
+      object.should_not respond_to(:bob=)
+    end
+
+    it "the new writer calls attribute_did_change with attribute name" do
+      object.replace_writer_for(:foo)
+      def object.attribute_did_change(attribute); @_called = attribute; end
+
+      object.instance_variable_get(:@_called).should be_nil
+      object.foo = 100
+      object.instance_variable_get(:@_called).should eq(:foo)
+    end
+  end
+
+  describe "#observe" do
+    it "handlers can be added to observe specific attributes" do
+      count = 0
+      object.observe(:foo) { count += 1 }
+
+      object.foo = 100
+      count.should eq(1)
+
+      object.foo = 150
+      count.should eq(2)
+    end
+
+    it "allows more than one handler to be added for an attribute" do
+      result = []
+      object.observe(:foo) { result << :first }
+      object.observe(:foo) { result << :second }
+
+      object.foo = 42
+      result.should eq([:first, :second])
+
+      object.foo = 3.142
+      result.should eq([:first, :second, :first, :second])
+    end
+  end
+
+  describe "#attribute_did_change" do
+    it "does not break for an object with no observers" do
+      object.attribute_did_change(:foo_bar)
+    end
+
+    it "does not break when given an attriubte with no observers (but another observable exists)" do
+      object.observe(:foo) {}
+      object.attribute_did_change(:bar)
+    end
+
+    it "passes new value to each handler" do
+      result = nil
+      object.observe(:foo) { |val| result = val }
+
+      object.foo = 42
+      result.should eq(42)
+
+      object.foo = 3.142
+      result.should eq(3.142)
+    end
+  end
+end
