d8425e06c0ae825f32caf4ccb34b12e211a6197b
Adam Beynon <adam.beynon@gmail.com>
2014-05-30 20:11:28 +0100

    Clean up specs and remove some duplication

--- spec/dom_compiler/if_binding_spec.rb
+++ spec/dom_compiler/if_binding_spec.rb
@@ -3,24 +3,25 @@ require 'spec_helper'
 describe Vienna::DOMCompiler do
   describe "if binding" do
     let(:view) { BoundView.new }
+    let(:html) { %(<div data-bind-if="loaded"></div>) }
 
     it "hides dom element when initially falsy" do
       view.loaded = false
-      view.run_bindings '<div data-bind-if="loaded"></div>'
+      view.run_bindings html
 
       view.element.css(:display).should == 'none'
     end
 
     it "does not hide dom element when initially truthy" do
       view.loaded = true
-      view.run_bindings '<div data-bind-if="loaded"></div>'
+      view.run_bindings html
 
       view.element.css(:display).should_not == 'none'
     end
 
     it "hides dom element visibility if attribute changes" do
       view.loaded = true
-      view.run_bindings '<div data-bind-if="loaded"></div>'
+      view.run_bindings html
 
       view.loaded = false
       view.element.css(:display).should == 'none'
